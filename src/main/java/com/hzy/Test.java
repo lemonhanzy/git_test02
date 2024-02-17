0f243c427982bf1b6907d33ea6dfdbe4c85062d0
hzy <1097212534@qq.com>
2024-02-17 08:39:15 +0800

    hotfixversion0.0.1

--- src/main/java/com/hzy/Test.java
+++ src/main/java/com/hzy/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git111");
         System.out.println("hello git222");
+        System.out.println("hot-fix test111");
     }
 }
